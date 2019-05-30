4d1499cfc20e1ae5298f9d2b05bc3cb10cc873a4
Fabiola <fabiolaalvarado11@gmail.com>
2019-05-30 11:05:37 -0600

    Cambios en PhotoGalleryActivity,photoGalleryFragment,SingleFragmentActivity

--- app/src/main/java/com/bignerdranch/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/bignerdranch/photogallery/PhotoGalleryActivity.java
@@ -3,8 +3,11 @@ package com.bignerdranch.photogallery;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
+
 
     @Override
     protected Fragment createFragment() {
